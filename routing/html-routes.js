c2c2b85d8aadd086aff5c853a68de3af37ddef22
Donovan Chavez <chavezdon17@gmail.com>
2020-11-28 21:36:41 -0600

    updated routes

---
+++ routing/html-routes.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+module.exports = function (app) {
+  app.get("notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../notes.html"));
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+};
