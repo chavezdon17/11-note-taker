e3ad5f38eb549e9a9305dc97ce66005abba1caa9
Donovan Chavez <chavezdon17@gmail.com>
2020-12-01 00:44:25 -0600

    adding and deleting notes

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,31 +1,56 @@
 //using node callbacks
+const { response } = require("express");
 const fs = require("fs");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+
+const notes = JSON.parse(response);
+const noteReq = req.body;
+const newNote = notes.length + 1;
+const addedNote = {
+  id: newNote,
+  title: noteReq.title,
+  text: noteReq.text,
+};
+const deleteNote = req.params.id;
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
     fs.readFile("db.json", "utf8", function (error, data) {
-      // error ? console.error(error) : console.log(data)
       res.json(JSON.parse(data));
     });
   });
 
   //adding /api/notes route
   app.post("/api/notes", function (req, res) {
-    let newNote = req.body;
-    notes.push(newNote);
-    updateDb();
-    return console.log("Added new note: " + newNote.title);
-  });
+    fs.readFile(
+      path.join(__dirname, "db.json"),
+      "utf8",
+      function (error, response) {
+        if (error) {
+          console.log(error);
+        }
+        notes.push(addedNote);
+        res.json(addedNote);
+        fs.writeFile(
+          path.join(__dirname, "db.json"),
+          JSON.stringify(notes, null, 2),
+          function (err) {
+            if (err) throw err;
+          }
+        );
+      }
+    );
 
-  app.get("/api/notes/id", function (req, res) {
-    res.json(notes[req.params.id]);
-  });
-
-  app.delete("/api/notes/id", function (req, res) {
-    notes.splice(req.params.id, 1);
-    updateDb();
-    console.log("Deleted note with id " + req.params.id);
+    app.delete("/api/notes/id", function (req, res) {
+      fs.readFile("db.json", "utf8", function (error, res) {
+        if (error) {
+          console.log("Deleted note with id " + req.params.id);
+        }
+        let notes = JSON.parse(response);
+        {
+          res.json(notes.splice(deleteNote - 1));
+        }
+      });
+    });
   });
 };
